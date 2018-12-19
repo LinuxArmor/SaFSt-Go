6e0d90fb5c2ab4c0ade5867bc07dbc422a6293ff
dondish <odedshapira1@gmail.com>
2018-12-19 19:41:13 +0200

    Me trying to figure what to do

--- fs/fs.go
+++ fs/fs.go
@@ -1,1 +1,13 @@
 package fs
+
+import "github.com/hanwen/go-fuse/fuse"
+
+type FileSystem struct {
+	fuse.RawFileSystem
+}
+
+func NewFileSystem() *FileSystem {
+	return &FileSystem{
+		RawFileSystem: fuse.NewDefaultRawFileSystem(),
+	}
+}

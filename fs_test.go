f7a056711e284527b931fdb92391fc10ab420ba7
dondish <odedshapira1@gmail.com>
2019-04-08 09:03:34 +0300

    Travis test

--- fs_test.go
+++ fs_test.go
@@ -1,14 +1,11 @@
-package tests
+package main
 
 import (
-	fs ".."
 	"bazil.org/fuse"
 	fs2 "bazil.org/fuse/fs"
 	"fmt"
-	"log"
 	"os"
 	"path"
-	"syscall"
 	"testing"
 )
 
@@ -16,34 +13,8 @@ const (
 	testFolder = "/tmp/entry"
 )
 
-func checkDirectory(path string) {
-	s, err := os.Stat(path)
-	if os.IsNotExist(err) { // Check if the folder exists
-		mkdirerr := os.Mkdir(path, 0700) // if not create it
-		if mkdirerr != nil {
-			log.Fatalf("Cannot open and create required folder: %s", path)
-		}
-		return
-	}
-	if err != nil { // Any other error should not happen but just in case
-		log.Fatalf("Cannot open and create required folder: %s", path)
-		return
-	}
-	if s.Mode().Perm() != 0700 { // Permissions needs to be as that for optimal security
-		if chmoderr := os.Chmod(path, os.ModeDir|0700); chmoderr != nil {
-			log.Fatalf("Cannot change permissions to  %s", path)
-		}
-	}
-
-	if fi := s.Sys().(*syscall.Stat_t); fi.Uid != 0 || fi.Gid != 0 { // Permissions needs to be as that for optimal security
-		if chownerr := os.Chown(path, 0, 0); chownerr != nil {
-			log.Fatalf("Cannot change ownership of  %s", path)
-		}
-	}
-}
-
 func startFileSystem(c *fuse.Conn) {
-	err := fs2.Serve(c, fs.NewFileSystem(testFolder))
+	err := fs2.Serve(c, NewFileSystem(testFolder))
 	if err != nil {
 		return
 	}
@@ -57,10 +28,10 @@ func TestMain(m *testing.M) {
 		panic("Couldn't create the test folder!")
 	}
 
-	checkDirectory(fs.DbFolder)
-	checkDirectory(fs.FileFolder)
+	checkDirectory(DbFolder)
+	checkDirectory(FileFolder)
 
-	err := fs.SetUpRoot()
+	err := SetUpRoot()
 
 	if err != nil {
 		panic(err)
@@ -112,7 +83,7 @@ func TestDir_Attr(t *testing.T) {
 		t.Errorf("Can't check the attributes of the test folder: %s", err)
 	}
 
-	fi2, err := os.Stat(fs.FileFolder)
+	fi2, err := os.Stat(FileFolder)
 
 	if err != nil {
 		t.Errorf("Can't check the attributes of the file folder: %s", err)
@@ -139,7 +110,7 @@ func TestDir_Mkdir(t *testing.T) {
 		t.Error("The new folder doesn't have the file mode given")
 	}
 
-	if fi, err := os.Stat(path.Join(fs.FileFolder, "new")); err != nil {
+	if fi, err := os.Stat(path.Join(FileFolder, "new")); err != nil {
 		t.Errorf("Errors getting attributes about the new folder in the file folder: %s", err)
 	} else if fi.Mode() != os.ModeDir|0700 {
 		t.Errorf("The new folder in the file folder doesn't have the file folder file mode")

97fd83fd83ad2dc07dbc9741f84455a39a250175
alwin <alpriven@gmail.com>
2019-04-22 20:19:26 +0300

    Fixed logic.go and tested on a.out

--- analyzing/logic.go
+++ analyzing/logic.go
@@ -3,17 +3,40 @@ package analyzing
 import (
 	"debug/elf"
 	"fmt"
+	"github.com/syndtr/goleveldb/leveldb"
+	"strconv"
 )
 
 func CheckPermissions(elf_file *elf.File) int {
-	var libs []string
+	//var libs []string
 	var syms []elf.ImportedSymbol
+	var ret int
 
 	var err error
-	libs, err = elf_file.ImportedLibraries()
+	//libs, err = elf_file.ImportedLibraries() in case we need list of imported libraries
+	db, err := leveldb.OpenFile("/usr/local/var/safst/perm", nil)
 	syms, err = elf_file.ImportedSymbols()
 	fmt.Println(err)
-	fmt.Printf("%v\n", libs)
-	fmt.Printf("syms: %v\n", syms)
-	return 0
+
+	for i := 0; i < len(syms); {
+		cur_sym := syms[i]
+		if cur_sym.Name == "__libc_start_main" {
+			i++
+			continue
+		}
+		cur_func := cur_sym.Name + "@" + cur_sym.Library
+		fmt.Println(cur_func)
+		var byte_per []byte
+		var per int
+
+		byte_per, _ = db.Get([]byte(cur_func), nil)
+		if len(byte_per) == 0 {
+			per = 64
+		} else {
+			per, _ = strconv.Atoi(string(byte_per))
+		}
+		ret = ret | per
+		i++
+	}
+	return ret
 }
--- analyzing/logic_test.go
+++ analyzing/logic_test.go
@@ -1,1 +1,13 @@
 package analyzing
+
+import (
+	"debug/elf"
+	"fmt"
+)
+
+func main() {
+	err := InitDB()
+	file, err := elf.Open("a.out")
+	fmt.Print(err)
+	fmt.Println(CheckPermissions(file)) // should return 72 because socket is unknown (64) and printf is 8
+}
